c17b68551e936438db5d726c7f59afc2347a2d83
Preston Osoro <prestonosoro56@gmail.com>
2025-04-09 01:15:27 +0300

    feat: add Book model with author relationship
    - Introduced `Book` model with fields for title, author (foreign key to Author model), description, and published date.
    - Ensured proper relationship between Book and Author models.

---
+++ books/models/book.py
@@ -0,0 +1,11 @@
+from django.db import models
+from authors.models.authors import Author
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+    published_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
