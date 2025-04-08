93ca80302bf45d9fb45386a9cf3702e118ae2094
Preston Osoro <prestonosoro56@gmail.com>
2025-04-09 01:28:00 +0300

    chore: remove obsolete files from library module

--- library/utils.py
+++
@@ -1,8 +0,0 @@
-from .models import Author
-
-def get_author_id(author_name):
-    try:
-        author = Author.objects.get(name=author_name)
-        return author.id
-    except Author.DoesNotExist:
-        return None
