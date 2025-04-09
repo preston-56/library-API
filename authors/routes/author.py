451df471e9335676e291d4724841b835f92ecc22
Preston Osoro <prestonosoro56@gmail.com>
2025-04-09 14:48:54 +0300

    feat(authors): add Swagger documentation for Author list endpoint
    - Integrated Swagger using `swagger_auto_schema` to document the `AuthorViewSet`'s list endpoint.
    - Added operation description to provide details in the API docs for retrieving a list of authors.

--- authors/routes/author.py
+++ authors/routes/author.py
@@ -2,8 +2,13 @@ from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from authors.models.authors import Author
 from authors.serializers.author import AuthorSerializer
+from drf_yasg.utils import swagger_auto_schema
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
     permission_classes = [AllowAny]
+
+    @swagger_auto_schema(operation_description="Retrieve a list of all authors")
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
