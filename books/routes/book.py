cde765a3fdba802d64eebbfd03c84019aa74334f
Preston Osoro <prestonosoro56@gmail.com>
2025-04-09 14:50:38 +0300

    feat(books): add Swagger documentation for Book list endpoint with search filter
    - Integrated Swagger using `swagger_auto_schema` to document the `BookViewSet`'s list endpoint.
    - Added an optional `search` query parameter to filter books by title or author name.
    - Provided detailed operation description in the API docs for enhanced clarity.

--- books/routes/book.py
+++ books/routes/book.py
@@ -1,19 +1,35 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from django.db.models import Q
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
 from books.models.book import Book
 from books.serializers.book import BookSerializer
-
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [AllowAny]
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        search = self.request.query_params.get("search", None)
-        if search:
-            queryset = queryset.filter(
-                Q(title__icontains=search) | Q(author__name__icontains=search)
+    @swagger_auto_schema(
+        operation_description="List books. Optional `search` param to filter by title or author name.",
+        manual_parameters=[
+            openapi.Parameter(
+                'search',
+                openapi.IN_QUERY,
+                description="Search by book title or author name",
+                type=openapi.TYPE_STRING
             )
-        return queryset
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+            return super().list(request, *args, **kwargs)
+
+    def get_queryset(self):
+            queryset = super().get_queryset()
+            search = self.request.query_params.get("search", None)
+            if search:
+                queryset = queryset.filter(
+                    Q(title__icontains=search) | Q(author__name__icontains=search)
+                )
+            return queryset
