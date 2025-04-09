4b45bb2ea747ddcc21186c2be567f0c70e2cf6ec
Preston Osoro <prestonosoro56@gmail.com>
2025-04-09 14:54:05 +0300

    feat: Configure Swagger UI and Redoc for API documentation
    - Set up Swagger and Redoc documentation using `drf-yasg`.
    - Created schema view with metadata (title, version, contact) for the API.
    - Added routes for serving Swagger UI (`/swagger/`) and Redoc UI (`/redoc/`).
    - Included support for JSON and YAML formats via `/swagger.json` and `/swagger.yaml`.
    - Integrated static file serving for Swagger UI in development mode.
    - Enhanced the API documentation with clear versioning and contact information.

--- library_api/urls.py
+++ library_api/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
+
+# Swagger
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
 # ViewSets
 from authors.routes.author import AuthorViewSet
@@ -17,11 +24,31 @@ router.register(r'authors', AuthorViewSet)
 router.register(r'books', BookViewSet)
 router.register(r'favorites', FavoriteViewSet)
 
+# Swagger schema view setup
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Library API",
+        default_version='v1',
+        description="API documentation for the Library project",
+        contact=openapi.Contact(email="prestonosoro56@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/register/', RegisterView.as_view(), name='register'),
     path('api/login/', LoginView.as_view(), name='login'),
     path('api/logout/', LogoutView.as_view(), name='logout'),
-    path('api/favorites/recommendations/', FavoriteViewSet.as_view({'get': 'recommendations'}), name='favorite-recommendations'),
+
+      # Swagger and Redoc
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+# Serve static files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
