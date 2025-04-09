93d53662a9c056c2c33e4b3a9c4e703253051041
Preston Osoro <prestonosoro56@gmail.com>
2025-04-09 14:43:48 +0300

    feat(favorite): create Favorite model
    - Introduced the `Favorite` model with relationships to `User` and `Book`.
    - Ensured a unique constraint on the combination of `user` and `book` to prevent duplicate entries.
    - Added a string representation method for better readability.

---
+++ favorite/models/books.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+from books.models.book import Book
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'book')
+
+    def __str__(self):
+        return f'{self.user.username} - {self.book.title}'
