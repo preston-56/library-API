ea5d8c9cc42938988b5e42e223b5c428c2c8fb72
Preston Osoro <prestonosoro56@gmail.com>
2025-04-10 22:21:52 +0300

    feat(favorite): implement `get_or_create` for book and add book serializer
    - Updated `FavoriteSerializer` to include `get_or_create` for the `Book` model, ensuring that books are not duplicated.
    - Modified `create` method to first check if the book exists by title and author; if not, it creates a new book entry.
    - The `BookSerializer` now handles nested book data, ensuring proper creation of book entries when creating favorites.
    - This change ensures that the favorite creation process efficiently associates an existing book or creates a new one without duplication.

--- favorite/serializers/favorite.py
+++ favorite/serializers/favorite.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
+from books.models.book import Book
 from favorite.models.books import Favorite
 from books.serializers.book import BookSerializer
-
 class FavoriteSerializer(serializers.ModelSerializer):
     book = BookSerializer()
 
     class Meta:
         model = Favorite
         fields = ['id', 'user', 'book']
+
+    def create(self, validated_data):
+        book_data = validated_data.pop('book')
+        book, created = Book.objects.get_or_create(
+            title=book_data['title'],
+            author__name=book_data['author']['name'],  # Ensure the author exists
+            defaults=book_data
+        )
+        favorite = Favorite.objects.create(book=book, **validated_data)
+        return favorite
