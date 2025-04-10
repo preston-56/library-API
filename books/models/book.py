557df258a705dad9e4c9c8c9c636f6eb4cbfe9aa
Preston Osoro <prestonosoro56@gmail.com>
2025-04-10 21:24:34 +0300

    feat(models): add Book model with relationship to Author
    - established a relationship between the Book and Author models using a ForeignKey
    - set the database table for Book as 'library_book'

--- books/models/book.py
+++ books/models/book.py
@@ -3,9 +3,11 @@ from authors.models.authors import Author
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, related_name="books", on_delete=models.CASCADE)
     description = models.TextField(blank=True)
     published_date = models.DateField(null=True, blank=True)
 
+    class Meta:
+        db_table = 'library_book'
     def __str__(self):
         return self.title
